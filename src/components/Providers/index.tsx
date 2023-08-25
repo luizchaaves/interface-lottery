bbc4f15a6d4f760c5a39e3741e030352fba09bb1
Luiz Otávio <luizotavio0106@outlook.com>
2023-08-25 11:15:37 -0300

    feat: import react query and provider settings

--- src/components/Providers/index.tsx
+++ src/components/Providers/index.tsx
@@ -1,10 +1,20 @@
 import { PropsWithChildren } from 'react';
 import { ThemeProvider } from 'styled-components';
 
-import megasena from '../../styles/themes/megasena';
+import { LotteryProvider } from '../../contexts/LotteryContext';
+import { megasena } from '../../styles/themes';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const Providers = ({ children }: PropsWithChildren) => {
-  return <ThemeProvider theme={megasena}>{children}</ThemeProvider>;
+  const client = new QueryClient();
+
+  return (
+    <QueryClientProvider client={client}>
+      <LotteryProvider>
+        <ThemeProvider theme={megasena}>{children}</ThemeProvider>
+      </LotteryProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
