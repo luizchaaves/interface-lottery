c6980074e9d05f0200a72b2132ad437567e55e32
Luiz Otávio <luizotavio0106@outlook.com>
2023-08-25 14:10:40 -0300

    refactor: add title field in lottery context

--- src/contexts/LotteryContext.tsx
+++ src/contexts/LotteryContext.tsx
@@ -2,7 +2,9 @@ import { PropsWithChildren, createContext, useState } from 'react';
 import { LotteryType } from '../types/lottery-types';
 
 type ILotteryContext = {
+  title: string | null;
   filter: LotteryType;
+  setTitle: React.Dispatch<React.SetStateAction<string | null>>;
   setFilter: React.Dispatch<React.SetStateAction<LotteryType>>;
 };
 
@@ -10,9 +12,10 @@ export const LotteryContext = createContext<ILotteryContext | null>(null);
 
 export const LotteryProvider = ({ children }: PropsWithChildren) => {
   const [filter, setFilter] = useState<LotteryType>(LotteryType.MEGA);
+  const [title, setTitle] = useState<string | null>(null);
 
   return (
-    <LotteryContext.Provider value={{ filter, setFilter }}>
+    <LotteryContext.Provider value={{ title, filter, setTitle, setFilter }}>
       {children}
     </LotteryContext.Provider>
   );
