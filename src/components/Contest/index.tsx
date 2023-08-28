f8442c7915b8f50a552cf7e788dd88686172b8ab
Luiz Otávio <luizotavio0106@outlook.com>
2023-08-28 16:49:57 -0300

    fix: creating loading skeleton to display the contest number

--- src/components/Contest/index.tsx
+++ src/components/Contest/index.tsx
@@ -3,10 +3,11 @@ import { useLottery } from '../../hooks/useLottery';
 import useLotteryResult from '../../hooks/useLotteryResult';
 import { Container } from './styles';
 import { ApiResponse } from '../../types/api-types';
+import Skeleton from 'react-loading-skeleton';
 
 const Contest = () => {
   const [dataResult, setDataResult] = useState<ApiResponse>();
-  const { data } = useLotteryResult();
+  const { data, isLoading } = useLotteryResult();
   const { filter } = useLottery();
 
   useEffect(() => {
@@ -15,13 +16,15 @@ const Contest = () => {
 
   return (
     <Container>
-      {dataResult && (
-        <>
-          <p>Concurso</p>
-          <span>
-            {dataResult.concurso} – {dataResult.data}
-          </span>
-        </>
+      <p>Concurso</p>
+      {isLoading ? (
+        <div>
+          <Skeleton baseColor="#e6e6e6" highlightColor="#f5f5f5" width={100} />
+        </div>
+      ) : (
+        <span>
+          {dataResult?.concurso} – {dataResult?.data}
+        </span>
       )}
     </Container>
   );
